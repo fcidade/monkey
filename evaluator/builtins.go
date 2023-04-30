9eeda61e14702dcbba457369893d5b0edd39ddbc
Francisco Cidade <franciscocidadehomemjunior@gmail.com>
2023-04-30 14:20:59 -0300

    puts builtin

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,8 @@
 package evaluator
 
 import (
+	"fmt"
+
 	"github.com/fcidade/monkey-lang/object"
 )
 
@@ -97,6 +99,14 @@ var builtins = map[string]*object.Builtin{
 			copy(newElements, arr.Elements[:length])
 			newElements[length] = args[1]
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
